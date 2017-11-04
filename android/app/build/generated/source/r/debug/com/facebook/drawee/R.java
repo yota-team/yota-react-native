f3f23a535da257feb417bdcac1d9ee7ae77b6739
tezaharsony <teza.harsony230394@gmail.com>
2017-11-04 17:17:25 +0700

    marker dan direction

--- android/app/build/generated/source/r/debug/com/facebook/drawee/R.java
+++ android/app/build/generated/source/r/debug/com/facebook/drawee/R.java
@@ -8,7 +8,6 @@ package com.facebook.drawee;
 
 public final class R {
 	public static final class attr {
-		public static final int actualImageResource = 0x7f01007d;
 		public static final int actualImageScaleType = 0x7f01003a;
 		public static final int actualImageUri = 0x7f01007c;
 		public static final int backgroundImage = 0x7f01003b;
@@ -74,8 +73,7 @@ public final class R {
 		public static final int GenericDraweeHierarchy_roundingBorderPadding = 24;
 		public static final int GenericDraweeHierarchy_roundingBorderWidth = 22;
 		public static final int GenericDraweeHierarchy_viewAspectRatio = 1;
-		public static final int[] SimpleDraweeView = { 0x7f01007c, 0x7f01007d };
-		public static final int SimpleDraweeView_actualImageResource = 1;
+		public static final int[] SimpleDraweeView = { 0x7f01007c };
 		public static final int SimpleDraweeView_actualImageUri = 0;
 	}
 }
--- index.android.js
+++ index.android.js
@@ -1,4 +1,95 @@
-import { AppRegistry } from 'react-native';
-import MapTest from './App';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions
+} from 'react-native';
 
-AppRegistry.registerComponent('maptest', () => MapTest);
+import MapView from 'react-native-maps';
+import Polyline from '@mapbox/polyline';
+
+export default class RnDirectionsApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      coords: [],
+      coordinate: {
+        latitude:-6.2349,
+        longitude:106.9896
+      },
+      coordinate2: {
+        latitude:-6.2380,
+        longitude:106.9896
+      }
+    }
+  }
+
+  componentDidMount() {
+
+    this.getDirections("-6.2349, 106.9896", "-6.21462, 106.84513")
+  }
+
+  async getDirections(startLoc, destinationLoc) {
+        try {
+            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }`)
+            let respJson = await resp.json();
+            let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
+            let coords = points.map((point, index) => {
+                return  {
+                    latitude : point[0],
+                    longitude : point[1]
+                }
+            })
+            this.setState({coords: coords})
+            return coords
+        } catch(error) {
+            alert(error)
+            return error
+        }
+    }
+
+  render() {
+    console.log('ini coordinate', this.state.coordinate);
+    return (
+      <View>
+        <MapView style={styles.map} initialRegion={{
+          latitude:-6.21462,
+          longitude:106.84513,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}>
+
+        <MapView.Marker draggable
+        coordinate={this.state.coordinate}
+        onDragEnd={(e) => this.setState({ coordinate: e.nativeEvent.coordinate })} />
+
+        <MapView.Marker draggable
+        coordinate={this.state.coordinate2}
+        onDragEnd={(e) => this.setState({ coordinate2: e.nativeEvent.coordinate })}
+        pinColor={'#474744'} />
+
+        <MapView.Polyline
+            coordinates={this.state.coords}
+            strokeWidth={2}
+            strokeColor="red"/>
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+});
+
+AppRegistry.registerComponent('maptest', () => RnDirectionsApp);
